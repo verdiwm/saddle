b1e32facbab09619a2d3b64cbe1677bc2bbe8438
Matilde Morrone <contact@morrone.dev>
2025-02-26 22:59:05 +0100

    Implement an initial higher level wrapper

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,93 @@
+use std::os::fd::OwnedFd;
+
+use futures_core::Stream;
+use login1::{manager::ManagerProxy, seat::SeatProxy, session::SessionProxy};
+use tokio_stream::StreamExt;
+use tracing::debug;
+use zbus::Connection;
+
 pub mod login1;
+
+pub type Result<T, E = Error> = std::result::Result<T, E>;
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error("{0}")]
+    Zbus(#[from] zbus::Error),
+}
+
+#[derive(Clone)]
+pub struct Seat {
+    session: SessionProxy<'static>,
+    seat: SeatProxy<'static>,
+    seat_name: String,
+}
+
+impl Seat {
+    pub async fn new() -> Result<Self> {
+        debug!("Connecting to system bus");
+        let connection = Connection::system().await?;
+
+        debug!("Creating manager proxy");
+        let manager = ManagerProxy::new(&connection).await?;
+
+        let session = manager.get_session_by_pid(std::process::id()).await?;
+
+        debug!("Creating session proxy");
+        let session = SessionProxy::new(&connection, session).await?;
+
+        debug!("Trying to take control of the session");
+        session.take_control(false).await?;
+
+        let (seat_name, seat_object) = session.seat().await?;
+
+        let seat = SeatProxy::new(&connection, seat_object).await?;
+
+        Ok(Self {
+            session,
+            seat,
+            seat_name,
+        })
+    }
+
+    pub async fn active_stream(&self) -> impl Stream<Item = Result<bool>> {
+        let active_changed = self.session.receive_active_changed().await;
+
+        active_changed.then(|prop| async move { prop.get().await.map_err(Error::Zbus) })
+    }
+
+    pub fn seat_name(&self) -> &str {
+        self.seat_name.as_str()
+    }
+
+    pub async fn release_session(&self) -> Result<()> {
+        self.session.release_control().await?;
+
+        Ok(())
+    }
+
+    pub async fn aquire_session(&self) -> Result<()> {
+        debug!("Trying to take control of the session");
+        self.session.take_control(false).await?;
+
+        Ok(())
+    }
+
+    pub async fn switch_session(&self, session: u32) -> Result<()> {
+        self.seat.switch_to(session).await?;
+
+        Ok(())
+    }
+
+    pub async fn open_device(&self, major: u32, minor: u32) -> Result<OwnedFd> {
+        let (fd, _) = self.session.take_device(major, minor).await?;
+
+        Ok(fd.into())
+    }
+
+    pub async fn close_device(&self, major: u32, minor: u32) -> Result<()> {
+        self.session.release_device(major, minor).await?;
+
+        Ok(())
+    }
+}
